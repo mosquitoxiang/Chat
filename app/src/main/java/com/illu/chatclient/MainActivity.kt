e5f97707e1c60bc7b1f7d90a758ab8bf9bad9ae6
mos <337280502@qq.com>
2020-09-24 22:57:33 +0800

    a

--- app/src/main/java/com/illu/chatclient/MainActivity.kt
+++ app/src/main/java/com/illu/chatclient/MainActivity.kt
@@ -1,83 +1,124 @@
 package com.illu.chatclient
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import io.netty.bootstrap.Bootstrap
-import io.netty.channel.ChannelFuture
-import io.netty.channel.ChannelHandlerContext
-import io.netty.channel.ChannelInitializer
-import io.netty.channel.SimpleChannelInboundHandler
-import io.netty.channel.nio.NioEventLoopGroup
-import io.netty.channel.socket.SocketChannel
-import io.netty.channel.socket.nio.NioSocketChannel
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder
-import io.netty.handler.codec.LengthFieldPrepender
-import io.netty.handler.codec.LineBasedFrameDecoder
-import io.netty.handler.codec.string.StringDecoder
-import io.netty.handler.codec.string.StringEncoder
-import io.netty.util.CharsetUtil
+import com.permissionx.guolindev.PermissionX
+import com.zhihu.matisse.Matisse
+import com.zhihu.matisse.MimeType
+import com.zhihu.matisse.engine.impl.GlideEngine
+import com.zhihu.matisse.filter.Filter
 import kotlinx.android.synthetic.main.activity_main.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
-import java.util.*
-import kotlin.collections.ArrayList
 import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var adapter: MsgAdapter
     private var msgList = mutableListOf<Msg>()
+    private var mSelected = mutableListOf<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         EventBus.getDefault().register(this)
+        btnBind.setOnClickListener(this)
+        btnConnect.setOnClickListener(this)
+        btnSend.setOnClickListener(this)
+        imgSelect.setOnClickListener(this)
         msgList = ArrayList()
 
         adapter = MsgAdapter().apply {
             bindToRecyclerView(rv)
         }
+        //连接服务器
+//        thread {
+//            Client("192.168.31.240", 8899).start()
+//        }
+    }
 
-        thread {
-            Client("192.168.31.240", 8899).start()
-        }
-
-        btnBind.setOnClickListener {
-            val bindId = etSelfId.text.toString().trim()
-            if (bindId.isNotEmpty()) {
-                Client().bind(bindId)
+    override fun onClick(p0: View?) {
+        when(p0?.id) {
+            R.id.btnBind -> {
+                val bindId = etSelfId.text.toString().trim()
+                if (bindId.isNotEmpty()) {
+                    Client().bind(bindId)
+                }
             }
-        }
-
-        btnConnect.setOnClickListener {
-            val connectId = etConnectId.text.toString().trim()
-            if (connectId.isNotEmpty()) {
-                Client().connect(connectId)
+            R.id.btnConnect -> {
+                val connectId = etConnectId.text.toString().trim()
+                if (connectId.isNotEmpty()) {
+                    Client().connect(connectId)
+                }
             }
-        }
-
-        btnSend.setOnClickListener {
-            val connectId = etConnectId.text.toString().trim()
-            val msg = etContent.text.toString().trim()
-            if (msg.isNotEmpty() && connectId.isNotEmpty()) {
-                Client().sendMsg(connectId, msg, object : ResultCallback{
-                    override fun send(isSuccess: Boolean) {
-                        val msg = Msg(msg, Msg.TYPE_SEND)
-                        msgList.add(msg)
-                        adapter.setNewData(msgList)
-                        rv.smoothScrollToPosition(msgList.size-1)
+            R.id.btnSend -> {
+                val connectId = etConnectId.text.toString().trim()
+                val msg = etContent.text.toString().trim()
+                if (msg.isNotEmpty() && connectId.isNotEmpty()) {
+                    Client().sendMsg(connectId, msg, object : ResultCallback{
+                        override fun send(isSuccess: Boolean) {
+                            val msgObj = Msg(msg, Msg.TYPE_SEND)
+                            msgList.add(msgObj)
+                            adapter.setNewData(msgList)
+                            rv.smoothScrollToPosition(msgList.size-1)
+                        }
+                    })
+                }
+            }
+            R.id.imgSelect -> {
+                PermissionX.init(this)
+                    .permissions(android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .request { allGranted, _, _ ->
+                        if (allGranted) {
+                            Matisse.from(this)
+                                .choose(MimeType.ofAll())
+                                .countable(true)
+                                .addFilter(GifSizeFilter(320, 320, 5 * Filter.K * Filter.K))
+                                .maxSelectable(9)
+                                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+                                .thumbnailScale(0.85f)
+                                .imageEngine(GlideEngine())
+                                .showPreview(true)
+                                .forResult(REQUEST_CODE_CHOOSE);
+                        }
                     }
-                })
             }
+        }
+    }
+
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_CHOOSE && resultCode == Activity.RESULT_OK) {
+            mSelected = Matisse.obtainResult(data)
+            mSelected.forEach {
+                val msgObj = Msg(type = Msg.TYPE_SEND, picUri = it)
+                msgList.add(msgObj)
+            }
+            adapter.setNewData(msgList)
         }
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     fun onEvent(msg: String) {
-        val msg = Msg(msg, Msg.TYPE_RECEIVE)
-        msgList.add(msg)
+        val msgObj = Msg(msg, Msg.TYPE_RECEIVE)
+        msgList.add(msgObj)
         adapter.setNewData(msgList)
         rv.smoothScrollToPosition(msgList.size-1)
+    }
+
+    companion object {
+        const val REQUEST_CODE_CHOOSE = 2000
     }
 }
--- app/src/main/java/com/illu/chatclient/MsgAdapter.kt
+++ app/src/main/java/com/illu/chatclient/MsgAdapter.kt
@@ -1,12 +1,19 @@
 package com.illu.chatclient
 
+import android.net.Uri
+import android.os.Environment
 import android.view.Gravity
 import android.view.View
+import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.illu.chatclient.Msg.Companion.TYPE_RECEIVE
 import com.illu.chatclient.Msg.Companion.TYPE_SEND
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.item_msg.view.*
+import top.zibin.luban.Luban
+import top.zibin.luban.OnCompressListener
+import java.io.File
 
 class MsgAdapter(layoutId: Int = R.layout.item_msg) :
     BaseQuickAdapter<Msg, BaseViewHolder>(layoutId) {
@@ -14,6 +21,26 @@ class MsgAdapter(layoutId: Int = R.layout.item_msg) :
     override fun convert(holder: BaseViewHolder, item: Msg) {
         holder.run {
             itemView.run {
+                if (item.picUri != null) {
+                    Luban.with(context)
+                        .load(UriToPathUtil.getImageAbsolutePath(context, item.picUri))
+                        .ignoreBy(100)
+                        .setTargetDir(context.cacheDir.absolutePath)
+                        .setCompressListener(object : OnCompressListener{
+                            override fun onSuccess(file: File?) {
+                                Glide.with(context).load(file).into(imgSelect)
+                            }
+
+                            override fun onError(e: Throwable?) {
+                                
+                            }
+
+                            override fun onStart() {
+                                
+                            }
+
+                        })
+                }
                 if (item.type == TYPE_SEND) {
                     tvRight.setText(item.content)
                     tvLeft.visibility = View.GONE
@@ -28,7 +55,8 @@ class MsgAdapter(layoutId: Int = R.layout.item_msg) :
 
 data class Msg(
     val content: String? = "",
-    val type: Int = 0
+    val type: Int = 0,
+    val picUri: Uri? = null
 ) {
 
     companion object {
